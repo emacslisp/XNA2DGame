bffd829fa44debc22425a68acbe73702b3a268c2
emacslisp <wudi850620@hotmail.com>
2017-02-11 23:53:15 +1100

    make android testing with voice and start to

--- Android/PlatformReading/PlatformReading/Game1.cs
+++ Android/PlatformReading/PlatformReading/Game1.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -12,6 +13,16 @@ namespace PlatformReading
         //@note: change visual studio to release to save as apk file
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        private Texture2D testSprite;
+
+        private SoundEffect effect;
+        private SoundEffect effectBackground;
+
+        private Vector2 position = new Vector2(400, 240);
+
+        int speedX = 1;
+        int speedY = 1;
+
 
         public Game1()
         {
@@ -46,7 +57,12 @@ namespace PlatformReading
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            /*@example: load jpg as sprite - step 1: add 1.jpg into content.mgcb, build mgcb file
+             * */
+            this.testSprite = Content.Load<Texture2D>("1");
+
+            this.effect = Content.Load<SoundEffect>("explode");
+            this.effect.Play();
         }
 
         /// <summary>
@@ -70,6 +86,36 @@ namespace PlatformReading
 
             // TODO: Add your update logic here
 
+
+            KeyboardState state = Keyboard.GetState();
+
+            if (state.IsKeyDown(Keys.Left))
+            {
+                //MediaPlayer.Play(effect);
+                // do something here
+                effect.Play();
+            }
+
+            if (position.X > GraphicsDevice.Viewport.Bounds.Width - testSprite.Width)
+            {
+                speedX = -1;
+            }
+            else if (position.X < 0)
+            {
+                speedX = 1;
+            }
+
+            if (position.Y > GraphicsDevice.Viewport.Bounds.Height - testSprite.Height)
+            {
+                speedY = -1;
+            }
+            else if (position.Y < 0)
+                speedY = 1;
+
+            position.X += speedX;
+            position.Y += speedY;
+
+
             base.Update(gameTime);
         }
 
@@ -82,6 +128,15 @@ namespace PlatformReading
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            spriteBatch.Begin();
+            spriteBatch.Draw(testSprite, position, Color.White);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
